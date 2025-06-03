9733b14d9af3c51f534426c4ad46e35cb4224196
laibaFtima <laibafatimapak9@gmail.com>
2025-06-03 17:50:50 +0000

    datatypes

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,9 @@
+"use strict";
+//premitive
+//number,boolean, string 
+//symbol //unique
+
+//null=>typeof object
+//undefined => undefined 
+//nonpremitive
+//object
---
+++ .devcontainer/basics/variable.js
@@ -0,0 +1,10 @@
+const accId= 189;
+let emial="laib@gmail.com";
+var nam="laiba";
+city="lahr";
+// accId=455;
+// console.log(accId);
+emial="zainab@gmail.com";
+nam="imran";
+city="gjr";
+console.table([emial,nam,city]);
