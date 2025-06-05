35129339fe399d8c4e69e5c3050c2f4de0c89864
laibaFtima <laibafatimapak9@gmail.com>
2025-06-05 17:58:16 +0000

    datatypes summary

--- .devcontainer/basics/datatypes.js
+++ .devcontainer/basics/datatypes.js
@@ -11,6 +11,29 @@
 // let negval=-val;
 // console.log(negval);
 // console.log(1+2+"5");
-let val=+true;
-console.log(val);
+// let val=+true;
+// console.log(val);
 
+// console.log(null==0);
+// console.log(undefined>0);
+// console.log("2">=1);
+//premitive
+//call by value means copy 
+// boolean, number ,string , null , undefined, symbol , bigInt
+// const score=100;
+// const scoreValue=123.5
+
+// const islog= false;
+// const tempout= true;
+// let useremail=undefined;
+//symbol
+let id=Symbol('345');
+let id2=Symbol("345");
+console.log(id===id2);
+let big=13445323232324n;
+//nonpremitive 
+// call by refference means memory alllocate 
+//function , array , objects
+//function datattype is objectfunction 
+// null ki datatype object hoti h 
+//or null ki datatype aik bug h or obejct ko tag k liye 000 use kiya jata tha  starting me 1995 m 
