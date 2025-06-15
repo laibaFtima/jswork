27a2ee595975ce6754299b0f01ad69da4d0fa877
laibaFtima <laibafatimapak9@gmail.com>
2025-06-15 17:26:07 +0000

    objects

--- .devcontainer/basics/array.js
+++ .devcontainer/basics/array.js
@@ -12,12 +12,32 @@
 // let newa=[1,24,545,[24,3445,54,],[45,56,8,]];
 // const form=newa.flat(Infinity);
 // console.log(form);
-console.log(Array.isArray("laiba"));// ye poochny k liye k ye array h yaa ni 
-console.log(Array.from("mlaika")); //array me convert krny k liye 
-console.log(Array.from({
-    name:"laiba",
-}))// is trha se khali object de ga menion krna pry ga key yaa values ki array bnani 
-let s1=3433;
-let s2=34343;
-let s3=343;
-console.log(Array.of(s1,s2,s3));
+// console.log(Array.isArray("laiba"));// ye poochny k liye k ye array h yaa ni 
+// console.log(Array.from("mlaika")); //array me convert krny k liye 
+// console.log(Array.from({
+//     name:"laiba",
+// }))// is trha se khali object de ga menion krna pry ga key yaa values ki array bnani 
+// let s1=3433;
+// let s2=34343;
+// let s3=343;
+// console.log(Array.of(s1,s2,s3));
+let sym=Symbol("hello");
+let obj={
+      nme:"laiba", //object me keys ko b asa string liya jata h 
+      rollnum:150,
+      email:"laiba@gmail.com",
+      [sym]:"hell",
+};
+console.log(obj.email);
+console.log(obj["email"]); //is me double koma me keys likhty hn 
+//aghr symbol ko objectm e add kr na h to asy krty hn 
+console.log(obj[sym]);
+obj.email ="hitejr@gmail.com"
+//Object.freeze(obj);
+obj.email ="djfnsjksf" 
+console.log(obj);
+obj.greeting =function(){
+    console.log(`hell u hell ${this.nme}`);
+}
+console.log(obj.greeting());
+
