2483675514e19d33f4816829e1e8c860bfda8780
laibaFtima <laibafatimapak9@gmail.com>
2025-06-17 13:07:27 +0000

    function

---
+++ .devcontainer/basics/function.js
@@ -0,0 +1,16 @@
+//function new topic
+function add(num1 , num2){
+    if(num1||num2===undefined)
+    {
+        console.log("enter number");
+        return
+    }
+return num1+num2;
+}
+const result =add(2,"");    //aghr hum kisis value me stroe kry gy to  
+console.log(result);       //result me value is liye ni h ku k hum ne
+                      //  function ki value return ni krwai jb value return krwaty us 
+                       //us ko store krwaty hn ya print bhr function k 
+                       //aghr user valeu ki jghah string yaa koi or value pass kr dy to ye 
+                       //us ko b add kr dy ga hum idr types define ni ki is k liye 
+                       //condition lgani prti h 
